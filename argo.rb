c2d074e6f583b5d45f77ce28f9319b01c106ddcf
osfx <osfx@protonmail.ch>
2017-01-12 23:13:28 +0300

    Edit file

--- argo.rb
+++ argo.rb
@@ -1,12 +1,8 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Argo < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/osfx/argo/archive/v0.1.0.tar.gz"
-  sha256 "4fe50af48ed8dfc379d260b87ec0d6575ee2e2ba8a355c2661e58113255c040c"
+  desc "argo app"
+  homepage "https://github.com/osfx/argo"
+  url "https://github.com/osfx/argo/archive/v0.1.1.tar.gz"
+  sha256 "7be02f551bd2982206342a1d0254b702e539355b449dc256e267ccbf4687341b"
 
   # depends_on "cmake" => :build
 
@@ -14,24 +10,23 @@ class Argo < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
 
     # Remove unrecognized options if warned by configure
-    # system "./configure", "--disable-debug",
-    #                       "--disable-dependency-tracking",
-    #                       "--disable-silent-rules",
-    #                       "--prefix=#{prefix}"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
     system "make", "install" # if this fails, try separate make/make install steps
   end
-
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test argo`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
+  # `test do` will create, run in and delete a temporary directory.
+  #
+  # This test will fail and we won't accept that! It's enough to just replace
+  # "false" with the main program this formula installs, but it'd be nice if you
+  # were more thorough. Run the test with `brew test argo`. Options passed
+  # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+  #
+  # The installed folder is not in the path, so use the entire path to any
+  # executables being tested: `system "#{bin}/program", "do", "something"`.
+  system "false"
+end
 end

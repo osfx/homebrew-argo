1abe1886ce9bf31772ba93fb46c3a936bf157171
osfx <osfx@protonmail.com>
2017-01-13 01:35:01 +0400

    Update argo.rb

--- argo.rb
+++ argo.rb
@@ -1,8 +1,7 @@
 class Argo < Formula
-  desc "Argo app"
+  desc "Orgo app"
   homepage "https://github.com/osfx/argo"
-  url "https://github.com/osfx/argo/releases/download/0.1.1-beta/argo"
-  version "0.1.1"
+  url "https://github.com/osfx/argo/archive/v0.1.1.tar.gz"
   sha256 "7be02f551bd2982206342a1d0254b702e539355b449dc256e267ccbf4687341b"
 
   def install
@@ -10,6 +9,6 @@ class Argo < Formula
   end
 
   test do
-#     system ["argo", "-h"]
+    system "true"
   end
 end

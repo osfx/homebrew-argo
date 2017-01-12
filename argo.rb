196ca07ddd2c01a3fcf5842f462bd2c36bff4606
osfx <osfx@protonmail.ch>
2017-01-12 22:50:43 +0300

    Add shasum

--- argo.rb
+++ argo.rb
@@ -2,7 +2,7 @@ class Argo < Formula
   homepage "https://github.com/osfx/argo"
   url "https://github.com/osfx/argo/archive/v0.1.0.tar.gz"
   version "0.1.0"
-  # sha256 "7b08d37c1eff84b648f57468f86262ee0980e235da27b7c64a49d14b0639c935"
+  sha256 "80e4622eb2b70701c6e8d5fadfea7e1efcef245e553029a9b14a35c484ebed85"
 
   def install
     bin.install("argo")

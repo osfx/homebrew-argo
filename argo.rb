5745a527c6eaaddd45914ced97f14fb895e504ff
osfx <osfx@protonmail.ch>
2017-01-12 22:56:03 +0300

    Add Makefile

--- argo.rb
+++ argo.rb
@@ -1,14 +1,37 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
 class Argo < Formula
-  homepage "https://github.com/osfx/argo"
+  desc ""
+  homepage ""
   url "https://github.com/osfx/argo/archive/v0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "80e4622eb2b70701c6e8d5fadfea7e1efcef245e553029a9b14a35c484ebed85"
+  sha256 "4fe50af48ed8dfc379d260b87ec0d6575ee2e2ba8a355c2661e58113255c040c"
+
+  # depends_on "cmake" => :build
 
   def install
-    bin.install("argo")
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
   end
 
   test do
-    system ["argo", "-h"]
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test argo`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
